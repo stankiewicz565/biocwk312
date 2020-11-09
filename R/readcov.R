db9294f6d14629b74f86e22292ed9f85f1d57556
Vince Carey <stvjc@channing.harvard.edu>
2020-11-09 15:27:36 -0500

    TnT project in place

--- R/readcov.R
+++ R/readcov.R
@@ -26,23 +26,30 @@ read_covg_by_symbol = function(sym, bamf, radius=0) {
 }
 
 #' produce a TnT::LineTrack with coverage information for a selected gene+radius
+#' @importFrom S4Vectors runLength runValue
 #' @param sym a gene symbol
 #' @param bamf a path to indexed BAM file
 #' @param radius numeric(1) defaulting to zero, upstream/downstream extra reach
+#' @param \dots passed to LineTrack, so can include color, etc.
 #' @examples
 #' if (requireNamespace("RNAseqData.HNRNPC.bam.chr14")) {
 #'   f1 = RNAseqData.HNRNPC.bam.chr14::RNAseqData.HNRNPC.bam.chr14_BAMFILES[1]
-#'   linetrack_covg_by_symbol("HNRNPC", f1)
+#'   linetrack_covg_by_symbol("HNRNPC", f1, color="gray")
 #' }
 #' @export
-linetrack_covg_by_symbol = function(sym, bamf, radius, ...) {
-  myr = read_covg_by_symbol(sym, bamf, radius)
-  interp = rep(runValue(myr), runLength(myr))
-  addrs = seq_len(length(interp))
-  left = min(which(interp>0))
-  right = max(which(interp>0))
-  counts = interp[left:right]
-  pos = addrs[left:right]
+linetrack_covg_by_symbol = function(sym, bamf, radius=0, ...) {
+  myr = read_covg_by_symbol(sym, bamf, radius=0)
+#  interp = rep(runValue(myr), runLength(myr))
+#  addrs = seq_len(length(interp))
+#  left = min(which(interp>0))
+#  right = max(which(interp>0))
+#  counts = interp[left:right]
+#  pos = addrs[left:right]
+  pos = cumsum(runLength(myr))
+  counts = runValue(myr)
+  last = length(counts)
+  pos = pos[-last]
+  counts = counts[-last]
   chr = genes_df_hg19[sym,]$seqnames
   gr = GenomicRanges::GRanges(chr, IRanges(pos,width=1))
   gr$value = counts
